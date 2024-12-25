6bea4bccf3f0e8119dad17e17cfba4a78dbbda67
Karim Ajrir <ajrir_karim@outlook.com>
2024-12-25 23:45:58 +0100

    Changed the computerChoice
    So i did go back so the computerchoice really returns a string

--- index.js
+++ index.js
@@ -1,28 +1,40 @@
 function getComputerChoice() {
     
-    const computerChoice = Math.floor(Math.random() * 3 + 1);
+    const randomChoice = Math.floor(Math.random() * 3 + 1);
+    let computerChoice = '';
     
-    if (computerChoice === 1){
-        return console.log("Computer chose Paper!")
-    } else if (computerChoice === 2){
-        return console.log("Computer chose Scissors!")
-    } else if (computerChoice === 3) {
-        return console.log("Computer chose Rock!")
+    if (randomChoice === 1){
+        computerChoice = 'paper';
+    } else if (randomChoice === 2){
+        computerChoice = 'scissors';
+    } else if (randomChoice === 3) {
+        computerChoice = 'rock';
     }
+    return computerChoice;
 }
+
 
 function getHumanChoice(){
     const myChoice = parseInt(prompt("What do you choose?\nPaper = 1, Scissors = 2, Rock = 3", 1));
-    let selectedChoice = 0;
+    let humanChoice = 0;
     
     if (myChoice === 1){
-        selectedChoice = 1;
+        humanChoice = 1;
         return console.log("You selected Paper!");
     } else if (myChoice === 2){
-        selectedChoice = 2;
+        humanChoice = 2;
         return console.log("You selected Scissors!");
     } else if (myChoice === 3){
-        selectedChoice = 3;
+        humanChoice = 3;
         return console.log("You selected Rock!");
     }
+
+    return humanChoice;
+}
+
+let humanScore = 0;
+let computerScore = 0;
+
+function playRound(humanChoice, computerChoice ){
+
 }
